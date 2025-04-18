546a6cbcd353ac83f61f21fdcb50c2a60302ed59
Sana-payzang <148889104+Sana-payzang@users.noreply.github.com>
2025-04-18 10:09:36 -0700

    Add files via upload

---
+++ assets/header-tag.php
@@ -0,0 +1,10 @@
+ <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+
+
+<link href="/apply/assets/style.css" rel="stylesheet">
+<script src="/apply/assets/knockout.js"></script>
+  
